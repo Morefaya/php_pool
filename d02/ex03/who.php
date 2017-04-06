a4bd3954db23bff9a06b9bb33859309dba613641
Jean-charles CAZAKO <jcazako@e1r4p3.42.fr>
2017-04-06 13:45:38 +0200

    adding d02/ex03

--- d02/ex03/who.php
+++ d02/ex03/who.php
@@ -3,9 +3,26 @@
 	if (($fd = fopen("/var/run/utmpx", "r")) == false)
 		exit;
 	date_default_timezone_set("Europe/Paris");
-	while ($line = fread($fd, 628))
+	$user = array();
+	$line = array();
+	$time = array();
+	$data = array();
+	$i = 0;
+	while ($buffer = fread($fd, 628))
 	{
-		echo $line."\n";
+		$data = unpack("a256user/a4id/a32line/ipid/itype/I2time/a256host/i16pad", $buffer);
+		if ($data["type"] == 7)
+		{
+			$user[$i] = $data["user"];
+			$line[$i] = $data["line"];
+			$time[$i] = $data["time1"];
+			$i++;
+		}
+	}
+	for ($j = 0; $j < $i; $j++)
+	{
+		$date = date("M  j H:i", $time[$j]);
+		echo $user[$j]."  ".$line[$j]."  ".$date."\n";
 	}
 	fclose($fd);
 ?>
